a43a0938a780489565b85c0b547f609428f3f60d
matteo <matteo-ferrante@hotmail.it>
2021-09-10 17:29:16 +0200

    Initial commit

---
+++ nn/vq_vae.py
@@ -0,0 +1,9 @@
+from tensorflow import keras
+from tensorflow.keras.layers import *
+import tensorflow_probability as tfp
+import tensorflow as tf
+from tensorflow.data import AUTOTUNE
+import os
+from tensorflow.keras.models import Model
+
+
